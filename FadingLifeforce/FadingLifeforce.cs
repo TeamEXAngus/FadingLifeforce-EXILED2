5aff4c4fe85496def36f0b98000da507e99e17fd
Will Morris <teamexangus@gmail.com>
2021-06-14 22:41:28 +1000

    Incremented version counter (5head)

--- FadingLifeforce/FadingLifeforce.cs
+++ FadingLifeforce/FadingLifeforce.cs
@@ -14,7 +14,7 @@ namespace FadingLifeforce
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 0);
+        public override Version Version { get; } = new Version(1, 0, 1);
 
         private Handlers.Hurting hurting;
         private Handlers.EnteringPD enteringPD;
