9479d0b87bb1c87db37614cbe04af22a47c3bf1b
Will Morris <teamexangus@gmail.com>
2021-07-16 21:23:19 +1000

    Version counter istg

--- FadingLifeforce/FadingLifeforce.cs
+++ FadingLifeforce/FadingLifeforce.cs
@@ -15,7 +15,7 @@ namespace FadingLifeforce
         public override PluginPriority Priority { get; } = PluginPriority.Medium;
 
         public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 4);
+        public override Version Version { get; } = new Version(1, 0, 5;
 
         private Handlers.Hurting hurting;
         private Handlers.EnteringPD enteringPD;
